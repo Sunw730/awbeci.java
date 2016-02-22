414177a6cdc4a86f55bed0abc8467d439de7ed2e
zhangwei900808 <zhangwei900808@hotmail.com>
2016-02-22 22:07:23 +0800

    update main page

--- src/main/webapp/js/views/main.js
+++ src/main/webapp/js/views/main.js
@@ -6,35 +6,38 @@ $(function () {
 });
 
 function initDlg() {
-    $('.hoverUserName').hover(function (e) {
-        var id = $(this).attr('id');
-        var positon = $(this).position();
-        $('.userInfoDlg').css({
-            left: positon.left,
-            top: positon.top + 29
+    $('.container-center').on('mouseenter mouseleave',
+        function (event) {
+            var $findhovera = $(this).children().find('a.hoverUserName');
+            if (event.type == 'mouseenter') {
+                var id = $findhovera.attr('id');
+                var positon = $findhovera.position();
+                $('.userInfoDlg').css({
+                    left: positon.left,
+                    top: positon.top + 29
+                });
+
+                $.post('/json/getUserInfoDlg.json',
+                    {uid: id},
+                    function (data) {
+                        $("#content-info-img img").attr('src', data.user.avatarUrl);
+                        $('#dynamic-time').text(data.user.createDt);
+                        $("#content-info-username a").attr({
+                            href: data.user.name
+                        }).text(data.user.name);
+                        $('#followingCount').text(data.followingsCount);
+                        $('#followerCount').text(data.followersCount);
+                        $('#sitesCount').text(data.sitesCount);
+                        $('#sitesCountNavigation').attr('href', data.user.name + '/navigation');
+                        $('.userInfoDlg').fadeTo('fast', 1);
+                    }, 'json');
+            } else {
+                var $target = event.relatedTarget
+                $('.userInfoDlg').hide();
+            }
         });
 
-        $.post('/json/getUserInfoDlg.json',
-            {uid: id},
-            function (data) {
-                $("#content-info-img img").attr('src', data.user.avatarUrl);
-                $('#dynamic-time').text(data.user.createDt);
-                $("#content-info-username a").attr({
-                    href: data.user.name
-                }).text(data.user.name);
-                $('#followingCount').text(data.followingsCount);
-                $('#followerCount').text(data.followersCount);
-                $('#sitesCount').text(data.sitesCount);
-                $('#sitesCountNavigation').attr('href', data.user.name + '/navigation');
-                $('.userInfoDlg').fadeIn();
-            }, 'json');
-    }, function (e) {
-        $('.userInfoDlg').fadeOut('slow');
-    });
-//todo:事件代理
-    $('.userInfoDlg').hover(function (e) {
-        $('.userInfoDlg').show();
-    },function(e){
-        $('.userInfoDlg').hide();
-    });
+    //$('.userInfoDlg').on('mouseleave', function (e) {
+    //    $('.userInfoDlg').hide();
+    //});
 }
